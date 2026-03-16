a7ad8d22416582f52f0b21b96c3e406652b4eb50
tech-shrimp <384295076@qq.com>
2026-03-16 18:22:44 +0800

    chore(config): 清理项目配置文件并修复IDE模块设置
    - 删除了应用属性配置文件中的数据库连接配置
    - 移除了SQL初始化和种子数据脚本文件
    - 修复了编译器XML中的模块名称配置错误
    - 更新了VCS目录映射配置以解决Git跟踪问题

--- ${project.build.directory}/classes/sql/db_seed.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `user` (`username`, `password`, `nickname`, `education_email`, `role`, `status`)
-SELECT 'admin', '123456', 'admin', 'admin@nuist.edu.cn', 1, 1
-WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin');
