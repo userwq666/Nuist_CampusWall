1e55cf55764e068625856fb6a63f8fed31ecd180
13470945997 <384295076@qq.com>
2026-05-05 22:12:46 +0800

    chore(git): 忽略IDE与构建产物并清理已跟踪文件

--- target/classes/sql/db_seed.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `user` (`username`, `password`, `nickname`, `education_email`, `role`, `status`)
-SELECT 'admin', '$2a$10$X6jAkn5sPwNg5br0khmGs.DvqqZB1aDQL8citFvvxGVzhnbnzw8Hu', 'admin', 'admin@nuist.edu.cn', 1, 1
-WHERE NOT EXISTS (SELECT 1 FROM `user` WHERE `username` = 'admin');
-
